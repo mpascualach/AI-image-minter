9c4f650b565acc9328c5f6b7ad9d90313a9a2082
mpascualach <miguelpacheson@hotmail.es>
2023-07-02 12:28:14 +0300

    removed a few console.logs

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const [modalOpen, setModalOpen] = useState(true);
 
   const handleInputChange = (event) => {
-    console.log("API key currently: ", apiKey);
+    // console.log("API key currently: ", apiKey);
     setPromptText(event.target.value);
   };
 
@@ -38,13 +38,15 @@ const App = () => {
       body: JSON.stringify({ prompt: promptText }),
     };
 
-    console.log("API key was: ", apiKey);
+    // console.log("API key was: ", apiKey);
 
     fetch(apiUrl, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         data.data[0].promptText = promptText;
-        console.log("API data loaded: ", JSON.stringify(data));
+        data.data[0].generatedBy = "Dall-E";
+        data.data[0].apiUsed = apiUrl || "none";
+        // console.log("API data loaded: ", JSON.stringify(data));
         setLoading(false);
         setImageUrls(data.data.map((item) => item.url));
       })
@@ -91,68 +93,64 @@ const App = () => {
 
   return (
     <>
-      {/* <ApiKeyModal
+      <ApiKeyModal
         isOpen={modalOpen}
         onClose={() => setModalOpen(false)}
         onSubmit={handleModalSubmit}
-      /> */}
-      <div>
-        <div className="testRunContainer">
-          <Button className="testRunButton" onClick={handleTestApi}>
-            Test run
-          </Button>
-        </div>
+      />
 
-        <form onSubmit={handleSubmit}>
-          {loading ? (
-            <Spinner />
-          ) : imageUrls.length ? (
-            <>
-              <Card variant="outlined" sx={{ height: 600, width: 600 }}>
-                <CardMedia
-                  image={imageUrls[0]}
-                  sx={{ height: 600, width: 600 }}
-                  style={{ position: "relative" }}
-                >
-                  <Button
-                    className="refreshButton"
-                    onClick={() => restartForm()}
-                  >
-                    <RefreshIcon></RefreshIcon>
-                  </Button>
-
-                  <Button
-                    className="signButton"
-                    color="success"
-                    variant="contained"
-                  >
-                    Sign as NFT
-                  </Button>
-                </CardMedia>
-              </Card>
-            </>
-          ) : (
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              <TextField
-                autoFocus
-                margin="normal"
-                id="prompt"
-                label="Prompt"
-                fullWidth
-                value={promptText}
-                onChange={handleInputChange}
-              />
-              <Button
-                color="success"
-                variant="contained"
-                onClick={() => handleSubmit(promptText)}
-              >
-                Enter prompt
-              </Button>
-            </div>
-          )}
-        </form>
+      <div className="testRunContainer">
+        <Button className="testRunButton" onClick={handleTestApi}>
+          Test run
+        </Button>
       </div>
+
+      <form onSubmit={handleSubmit}>
+        {loading ? (
+          <Spinner />
+        ) : imageUrls.length ? (
+          <>
+            <Card variant="outlined" sx={{ height: 600, width: 600 }}>
+              <CardMedia
+                image={imageUrls[0]}
+                sx={{ height: 600, width: 600 }}
+                style={{ position: "relative" }}
+              >
+                <Button className="refreshButton" onClick={() => restartForm()}>
+                  <RefreshIcon></RefreshIcon>
+                </Button>
+
+                <Button
+                  className="signButton"
+                  color="success"
+                  variant="contained"
+                >
+                  Sign as NFT
+                </Button>
+              </CardMedia>
+            </Card>
+          </>
+        ) : (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <TextField
+              autoFocus
+              margin="normal"
+              id="prompt"
+              label="Prompt"
+              fullWidth
+              value={promptText}
+              onChange={handleInputChange}
+            />
+            <Button
+              color="success"
+              variant="contained"
+              onClick={() => handleSubmit(promptText)}
+            >
+              Enter prompt
+            </Button>
+          </div>
+        )}
+      </form>
     </>
   );
 };
